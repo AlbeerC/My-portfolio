2c1665a3017e399d59da008112d590e2760e3e87
Alberto Caminos <albeerc@gmail.com>
2025-02-12 10:45:27 -0300

    align sections vertically

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,19 +9,25 @@ function Contact () {
             <h3>Transformando ideas en realidad: comencemos a colaborar.</h3>
             <div className="cards">
                 <div className="card">
-                    <FaLinkedin />
-                    <h4>Linkedin</h4>
-                    <p>/alberto-caminos</p>
+                    <a href="https://www.linkedin.com/in/alberto-caminos-b73b0a190/" target="_blank" rel="noreferrer">
+                        <FaLinkedin />
+                        <h4>Linkedin</h4>
+                        <p>/alberto-caminos</p>
+                    </a>
                 </div>
                 <div className="card">
-                    <FaEnvelope />
-                    <h4>E-mail</h4>
-                    <p>caminosalbertodev@gmail.com</p>
+                    <a href="mailto:caminosalbertodev@gmail.com" target="_blank" rel="noreferrer">
+                        <FaEnvelope />
+                        <h4>E-mail</h4>
+                        <p>caminosalbertodev@gmail.com</p>
+                    </a>
                 </div>
                 <div className="card">
-                    <FaWhatsapp />
-                    <h4>Whatsapp</h4>
-                    <p>+54 2284697534</p>
+                    <a href="https://wa.me/542284697534" target="_blank" rel="noreferrer">
+                        <FaWhatsapp />
+                        <h4>Whatsapp</h4>
+                        <p>+54 2284697534</p>
+                    </a>
                 </div>
             </div>
         </section>
